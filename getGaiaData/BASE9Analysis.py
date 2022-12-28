7b46cb1e1e78d38ab41534f64109c78c19d7f8b4
Clairezy <clairezwicker98@gmail.com>
2022-12-28 15:32:39 -0600

    Reworked MegaCMD.ipynb, added analysis.py

---
+++ getGaiaData/BASE9Analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
+import astropy.units as units
+
+def readGaiaData(filepath: str) -> pd.DataFrame:
+    data = pd.read_csv(filepath, sep='\s+', skiprows=180)
+    data = data.rename(columns={'phot_g_mean_mag': 'G', 'phot_bp_mean_mag': 'G_BP', 'phot_rp_mean_mag': 'G_RP'})
+    return data[['source_id', 'G', 'G_BP', 'G_RP', 'ra', 'dec']]
+
+def findSeparation(dataframe: pd.DataFrame) -> pd.DataFrame:
+    c1 = SkyCoord(np.deg2rad(dataframe['ra']),np.deg2rad(dataframe['dec']),unit='rad')
+    c2 = SkyCoord(np.deg2rad(295.3250),np.deg2rad(40.19),unit='rad')
+    distance = c1.separation(c2).value
+    return distance 
